10886655ecc02f7a6f6910ac009279d91e51baee
Dheeraj Reddy <dreddy@MacBookPro.local>
2023-02-20 07:41:34 -0800

    Use tango-dark for now - nobold

--- emacs/init.el
+++ emacs/init.el
@@ -125,17 +125,19 @@
     (unbind-key "M-<" ggtags-mode-map)
     (unbind-key "M->" ggtags-mode-map))
 
+;; (use-package emacs
+;;    :config
+;;    (load-theme 'wombat)
+;;    (set-face-background 'default "#111")
+;;    (set-face-background 'cursor "#c96")
+;;    (set-face-background 'isearch "#c60")
+;;    (set-face-foreground 'isearch "#eee")
+;;    (set-face-background 'lazy-highlight "#960")
+;;    (set-face-foreground 'lazy-highlight "#ccc")
+;;    (set-face-foreground 'font-lock-comment-face "#fc0"))
 (use-package emacs
-   :config
-   (load-theme 'wombat)
-   (set-face-background 'default "#111")
-   (set-face-background 'cursor "#c96")
-   (set-face-background 'isearch "#c60")
-   (set-face-foreground 'isearch "#eee")
-   (set-face-background 'lazy-highlight "#960")
-   (set-face-foreground 'lazy-highlight "#ccc")
-   (set-face-foreground 'font-lock-comment-face "#fc0"))
-
+  :config
+  (load-theme 'tango-dark))
 
 (use-package markdown-mode
   :ensure t
